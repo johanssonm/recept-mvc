f28c49067da9d339f40f4fa153e455150f0d64e2
Mattias Johansson <mattiasj@protonmail.com>
2018-04-01 14:19:48 +0200

    Uppdaterade detaljcontroller

--- recept-mvc/recept-mvc/Controllers/ReceptController.cs
+++ recept-mvc/recept-mvc/Controllers/ReceptController.cs
@@ -11,6 +11,19 @@ namespace Recept.Controllers
 
         public ActionResult Detail()
         {
+            ViewBag.Title = "Hello from or Detail view";
+            ViewBag.PageTitle = "Spaghetti Bolognese";
+            ViewBag.ReceptID = 150;
+            ViewBag.Description = "<em>Best bolognese recipe!</em> Takes a long time but worth it. Veal easily substituted with more of other meats. Also veal Demi grace can be eliminated or substitute a beef broth & roux combo";
+            ViewBag.Ingredients = new string[]
+            {
+                "1/2 pound ground veal",
+                "freshly ground pepper",
+                "salt",
+                "grated nutmeg (Freshly, to taste)",
+                "1/2 cup dry white wine "
+            };
+
             return View();
         }
     }
