08ed58703e48045ed0f6714d34000716ee7b26c3
Unknown <mattiasj@protonmail.com>
2018-04-01 12:58:03 +0200

    La till en controller

---
+++ recept-mvc/recept-mvc/Controllers/ReceptController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Recept.Controllers
+{
+    public class ReceptController : Controller
+    {
+
+        public string Detail()
+        {
+
+            return "Hello world!";
+        }
+    }
+}
