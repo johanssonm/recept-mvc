1f23054db1e745af56bc016bced72c3a03e87e7b
Mattias Johansson <mattiasj@protonmail.com>
2018-04-01 15:27:57 +0200

    Upddaterade receptmodellen

---
+++ recept-mvc/recept-mvc/Models/Ingredienser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace recept_mvc.Models
+{
+    public class Ingredienser
+    {
+        private string Namn { get; set; }
+        private string Enhet { get; set; }
+    }
+}
---
+++ recept-mvc/recept-mvc/Models/Recept.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace recept_mvc.Models
+{
+    public class Recept
+    {
+        public int ReceptID { get; set; }
+        public string Rubrik { get; set; }
+        public string BeskrivningHTML { get; set; }
+        public Ingredienser[] Ingredienser { get; set; }
+        public bool Favorit { get; set; }
+
+        public string DisplayText
+        {
+            get { return Rubrik + " " + ReceptID; }
+        }
+
+       public string CoverImageFilename
+       {
+           get { return Rubrik.Replace(" ", "-").ToLower() + "-" + ReceptID.ToString();  }
+       }
+    }
+}
